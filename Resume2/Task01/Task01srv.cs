89d1d63d8071a6dc3446720215b3d042f6c4bccd
SheAnatoliy <wrkwww@yandex.ru>
2020-01-27 17:37:26 +0300

    AcceptTcpClient()

---
+++ Resume2/Task01/Task01srv.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Task01
+{
+	static class Server
+	{
+		static readonly TcpListener Listener = new TcpListener(IPAddress.Any, 8081);
+
+		static void Main(string[] args)
+		{
+			try
+			{
+				if (Listener != null)
+				{
+					Listener.Start();
+					while (true)
+					{
+						// Принимаем новых клиентов
+						Listener.AcceptTcpClient();
+					}
+				}
+
+			}
+			finally
+			{
+				if (Listener != null)
+					Listener.Stop();
+				else
+					Console.WriteLine("Ошибка запуска (Listener)..");
+			}
+		}
+	}
+}
